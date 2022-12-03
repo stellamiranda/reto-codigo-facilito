1e08451f66f295bf08601f8c21737890720f7db9
Sergio <me@sergiovp.dev>
2022-12-03 14:43:30 -0600

    feat: normalize empty string values to nil in task values

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,7 +7,15 @@ class Task < ApplicationRecord
   belongs_to :user
   has_one_attached :image, dependent: :destroy
   #before_save
-  #after_update 
+  #after_update
+
+  before_validation do
+    if title.blank?
+      self.title = nil
+    end
+
+    if description.blank?
+      self.description = nil
+    end
+  end
 end
-
-
